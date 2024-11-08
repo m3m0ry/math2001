0f5ec52f3b8fea0a70f76ffbbfceea2afaa761b5
Jan Hönig <jan.hoenig@fau.de>
2024-11-08 23:54:16 +0100

    Chapter 1

--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -12,8 +12,7 @@ example {a b : ℤ} (ha : a - 2 * b = 1) : a = 2 * b + 1 := by addarith [ha]
 
 example {x y : ℚ} (hx : x = 2) (hy : y ^ 2 = -7) : x + y ^ 2 = -5 :=
   calc
-    x + y ^ 2 = x - 7 := by addarith [hy]
-    _ = -5 := by addarith [hx]
+    x + y ^ 2 = -5 := by addarith [hx, hy]
 
 
 example {s t : ℝ} (h : t = 4 - s * t) : t + s * t > 0 := by addarith [h]
@@ -22,4 +21,7 @@ example {m n : ℝ} (h1 : m ≤ 8 - n) : 10 > m + n := by addarith [h1]
 
 
 -- Check that `addarith` can't verify this deduction!
-example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := sorry
+example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 :=
+  calc
+    w = (3 * w + 1) /3 - 1/3 := by ring
+    _ = 1 := by addarith[h1]
